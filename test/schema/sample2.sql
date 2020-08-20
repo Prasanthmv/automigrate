9eb756bb47fef506df506bc46a519d496e8bebeb
Prasanthmv <prasanth@citrusinformatics.com>
2020-08-20 14:23:48 +0530

    update sql script

--- test/schema/sample2.sql
+++ test/schema/sample2.sql
@@ -2,7 +2,7 @@
 CREATE TABLE accounts (
 	user_id serial PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
-	password VARCHAR ( 50 ) NOT NULL,
+	user_password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
 	created_on TIMESTAMP NOT NULL,
     last_login TIMESTAMP 
