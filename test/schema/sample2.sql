7df0d0011873fa5d60c302d6d67ff6be73ed79b3
Prasanthmv <prasanth@citrusinformatics.com>
2020-08-20 14:21:23 +0530

    update sql script

--- test/schema/sample2.sql
+++ test/schema/sample2.sql
@@ -7,3 +7,20 @@ CREATE TABLE accounts (
 	created_on TIMESTAMP NOT NULL,
     last_login TIMESTAMP 
 );
+
+-- step2
+CREATE TABLE roles(
+   role_id serial PRIMARY KEY,
+   role_name VARCHAR (255) UNIQUE NOT NULL
+);
+
+CREATE TABLE account_roles (
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+  grant_date TIMESTAMP,
+  PRIMARY KEY (user_id, role_id),
+  FOREIGN KEY (role_id)
+      REFERENCES roles (role_id),
+  FOREIGN KEY (user_id)
+      REFERENCES accounts (user_id)
+);
