4422e545c635574cf94a0b3689da0a0663b3d70e
Prasanthmv <prasanth@citrusinformatics.com>
2020-08-20 11:24:20 +0530

    update sql script

--- test/schema/sql.sql
+++ test/schema/sql.sql
@@ -11,6 +11,5 @@ create index t12a on t1 (a);
 create table t22 (a int primary key);
 
 -- Test2
---ALTER TABLE t1 ADD COLUMN fax VARCHAR,ADD COLUMN email VARCHAR;
 create table t1 (a int primary key, b text,fax VARCHAR,email VARCHAR);
 
