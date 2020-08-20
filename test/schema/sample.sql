6478b0fe1388a85c69cff8979cf0f3df685aa8aa
Prasanthmv <prasanth@citrusinformatics.com>
2020-08-20 11:44:03 +0530

    update sql script

--- test/schema/sample.sql
+++ test/schema/sample.sql
@@ -6,3 +6,4 @@ create table person (id int primary key, full_name text, age int);
 
 create table person (id int primary key, full_name text, age int, email varchar);
 
+create table person_address (id int primary key, pid int , street_1 varchar, street_2 varchar);
