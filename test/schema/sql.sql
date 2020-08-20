44e53c72492f841a0be7f3f32c0265cf3d7011a1
Prasanthmv <prasanth@citrusinformatics.com>
2020-08-20 11:19:21 +0530

    update sql script with alter query

--- test/schema/sql.sql
+++ test/schema/sql.sql
@@ -5,6 +5,10 @@ create table t1 (a int primary key, b text);
 create index t1a on t1 (a);
 create table t2 (a int primary key);
 
+-- Test1
 create table t12 (a int primary key, b text);
 create index t12a on t1 (a);
 create table t22 (a int primary key);
+
+-- Test2
+ALTER TABLE t1 ADD COLUMN fax VARCHAR,ADD COLUMN email VARCHAR;
